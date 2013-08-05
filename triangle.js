f57668a5672f3f574285e8bde913c732ef3150a7
Alec Jordan <alecbjordan@gmail.com>
2013-08-04 20:28:36 -0400

    tracking and triangle enemies

--- triangle.js
+++ triangle.js
@@ -25,20 +25,29 @@
 	p.makeShape = function () {
 		var g = this.triangleShape.graphics;
 		g.clear();
-		g.beginFill("green").drawCircle(0, 0, 15);
+		g.beginFill("green").drawPolyStar(0, 0, 15, 3, 0, 0);
 	}
 
 	p.tick = function(event) {
 		// move the triangle toward the jet
 		var target = window.jet;
 
-		this.x += 5;
-		this.y += 5;
+		var deltax = target.x - this.x;
+		var deltay = target.y - this.y;
+
+		var angle = Math.atan2(deltay, deltax);
+		var speed = 6;
+
+		this.x += speed * Math.cos(angle);
+		this.y += speed * Math.sin(angle);
 	}
 
 	p.inBounds = function() {
         return this.x > 0 && this.y > 0 && this.x <= window.canvasWidth && this.y <= window.canvasHeight;
     }
 
+    function square(x) {
+    	return x * x;
+    }
 	window.Triangle = Triangle;
 }(window))
